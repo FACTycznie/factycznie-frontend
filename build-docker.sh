33cccf6ddcc1653e0d05d77e9856c1b2dafa1693
Piotr Peczek <ppeczek@gmail.com>
2018-02-26 20:57:46 +0100

    dockerized

---
+++ build-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -t factycznie_frontend .
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 // server.js
+'use strict';
 
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
 
-app = express();
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+
+const path = require('path');
+const serveStatic = require('serve-static');
+
+const app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log('server started '+ port);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
